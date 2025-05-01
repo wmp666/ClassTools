a9537ff52b7f934c2d80f50ecb151e6d2dafb124
wmp666 <2134868121@qq.com>
2025-05-01 23:49:02 +0800

    特别的关闭方式

--- src/com/wmp/PublicTools/printLog/Log.java
+++ src/com/wmp/PublicTools/printLog/Log.java
@@ -2,10 +2,15 @@ package com.wmp.PublicTools.printLog;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Random;
 
 public class Log {
     private static final ArrayList<String> logInfList = new ArrayList<>();
@@ -43,18 +48,89 @@ public class Log {
     public static PrintLogStyle warn = new PrintLogStyle(LogStyle.WARN);
     public static PrintLogStyle error = new PrintLogStyle(LogStyle.ERROR);
 
-    static{
+    static {
         thread.setDaemon(true);
         thread.start();  // 确保启动线程
     }
 
-    public static void print(LogStyle style, String owner, String logInfo){
+    public static void exit(int status) {
+
+        //获取桌面大小
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+
+        JWindow window = new JWindow();
+        window.setSize(screenSize);
+        Container c = window.getContentPane();
+        c.setLayout(new BorderLayout());
+        c.setBackground(Color.BLACK);
+
+        window.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosed(WindowEvent e) {
+                System.exit(status);
+            }
+        });
+
+
+        String[] exitStrList = {
+                "愿此行，抵达■■",
+                "■■终将重逢",
+                "明天见",
+                "聚散终有时，山水有相逢。",
+                "樱花凋零时，方知一期一会。",
+                "当■有机会做出选择的时候，\n不要■■■后悔。",
+                "■■只得学着独立行走，\n在跌倒爬起中度过碌碌■■。\n但失败的■■同样是■■，\n■■有权品尝至最后。",
+                "每一次移动后发生的变化，\n每一个不经意间弹出的话语，\n都在呈现■■■■时的所见所感……\n虽不如■■■■，\n但创造出了足够的情感冲击。"
+        };
+        String exitStr = exitStrList[new Random().nextInt(exitStrList.length)];
+        if (exitStr.contains("\n")) {
+            exitStr = "<html>" + exitStr.replaceAll("\\n", "<br>") + "</html>";
+        }
+        //String result = "<html>" + exitStr.replaceAll("\\n", "<br>") + "</html>";
+        JLabel label = new JLabel(exitStr);// 创建标签
+        label.setForeground(Color.WHITE);
+        label.setFont(new Font("微软雅黑", Font.BOLD, 60));
+        label.setHorizontalAlignment(SwingConstants.CENTER);
+
+        c.add(label, BorderLayout.CENTER);
+        window.setVisible(true);
+
+
+
+        new Timer(10, new ActionListener() {
+            private float alpha = 255; // 使用浮点保证平滑过渡
+            private final float delta = 255 / (1000/10f); // 按1秒持续时间计算步长
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (alpha <= 0) {
+                    ((Timer)e.getSource()).stop();
+                    return;
+                }
+
+                alpha = Math.max(0, alpha - delta);
+                label.setForeground(new Color(255, 255, 255, 255 - (int)alpha));
+            }
+        }).start();
+
+
+
+        // 改为自动关闭窗口
+        new Timer(3000, e -> {
+            window.dispose();
+            System.exit(status);
+        }).start();
+
+
+    }
+
+    public static void print(LogStyle style, String owner, String logInfo) {
         Date date = new Date();
         DateFormat dateFormat = new SimpleDateFormat("MM.dd HH:mm:ss");
         String dateStr = dateFormat.format(date);
 
         String info;
-        switch (style){
+        switch (style) {
             case INFO -> {
 
                 info = "[" + dateStr + "]" +
@@ -65,7 +141,7 @@ public class Log {
                 logInfList.add(info);
             }
 
-            case WARN->{
+            case WARN -> {
 
                 info = "[" + dateStr + "]" +
                         "[警告]" +
@@ -75,7 +151,7 @@ public class Log {
                 logInfList.add(info);
             }
 
-            case ERROR->{
+            case ERROR -> {
 
                 info = "[" + dateStr + "]" +
                         "[错误]" +
@@ -93,9 +169,10 @@ public class Log {
     public static ArrayList<String> getLogInfList() {
         return logInfList;
     }
+
     public static void showLogDialog() {
         //dialog.removeAll();
-        JDialog dialog = new JDialog((Frame) null,false);
+        JDialog dialog = new JDialog((Frame) null, false);
         dialog.setTitle("日志");
         dialog.setSize(500, 600);
         dialog.setLocationRelativeTo(null);
