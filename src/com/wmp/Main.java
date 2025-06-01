f5136f903c5688db08e58aad727d0151bba6178c
wmp666 <2134868121@qq.com>
2025-06-01 21:29:03 +0800

    ->更新菜单文字

--- src/com/wmp/Main.java
+++ src/com/wmp/Main.java
@@ -28,7 +28,7 @@ public class Main{
     * d:只修复的问题,问题较少
     * e:测试版本号
      */
-    public static String version = "1.21.0";
+    public static String version = "1.21.1";
     public static String appName = "ClassTools";
     public static String author = "wmp";
 
