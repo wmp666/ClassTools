186f04cb4e9691a4af1ce71c9910ab7c42671047
无名牌 <2134868121@qq.com>
2025-07-30 08:02:47 +0800

    ->修改缓存清理页

--- src/com/wmp/classtools/infSet/panel/ClearTempPanel.java
+++ src/com/wmp/classtools/infSet/panel/ClearTempPanel.java
@@ -1,12 +1,10 @@
 package com.wmp.classTools.infSet.panel;
 
 import com.wmp.Main;
-import com.wmp.PublicTools.UITools.CTFont;
-import com.wmp.PublicTools.UITools.CTFontSizeStyle;
 import com.wmp.PublicTools.io.IOForInfo;
 import com.wmp.PublicTools.printLog.Log;
-import com.wmp.classTools.CTComponent.CTTextButton;
 import com.wmp.classTools.CTComponent.CTSetsPanel;
+import com.wmp.classTools.infSet.panel.tools.DataControlUnit;
 
 import javax.swing.*;
 import java.awt.*;
@@ -19,7 +17,7 @@ public class ClearTempPanel extends CTSetsPanel {
     public ClearTempPanel(String basicDataPath) {
         super(basicDataPath);
 
-        setName("清理临时文件");
+        setName("软件数据文件管理");
 
         try {
             initUI();
@@ -33,29 +31,43 @@ public class ClearTempPanel extends CTSetsPanel {
         this.setBackground(Color.WHITE);
         this.setLayout(new GridLayout(0, 1, 5, 5));
 
-        JPanel buttonPanel = new JPanel();
+        /*JPanel buttonPanel = new JPanel();
         buttonPanel.setBackground(Color.WHITE);
-        buttonPanel.setLayout(new FlowLayout(FlowLayout.LEFT, 5, 5));
-        buttonPanel.setBounds(0, 0, 400, 45);
+        buttonPanel.setLayout(new GridLayout(0,1));
+        buttonPanel.setBounds(0, 0, 400, 45);*/
 
-        buttonPanel.add(getButton("清除临时文件", Main.TEMP_PATH));
+        this.add(getControlUnit("临时文件", Main.TEMP_PATH, true,
+                new DataControlUnit("彩蛋文件", Main.TEMP_PATH + "EasterEgg", true,
+                        new DataControlUnit("视频文件", Main.TEMP_PATH + "EasterEgg\\video", true),
+                        new DataControlUnit("音频文件", Main.TEMP_PATH + "EasterEgg\\music", true)),
+                new DataControlUnit("网络文件下载缓存", Main.TEMP_PATH + "WebTemp", true),
+                new DataControlUnit("帮助文档缓存", Main.TEMP_PATH + "help", true)));
+        this.add(getControlUnit("数据文件", Main.DATA_PATH, false,
+                new DataControlUnit("日志", Main.DATA_PATH + "Log", true),
+                new DataControlUnit("屏保数据", Main.DATA_PATH + "ScreenProduct", false),
+                new DataControlUnit("值日数据", Main.DATA_PATH + "Duty", false),
+                new DataControlUnit("迟到数据", Main.DATA_PATH + "Att", false),
+                new DataControlUnit("插件", Main.DATA_PATH + "Cookie", false),
+                new DataControlUnit("个性化文件", Main.DATA_PATH + "setUp.json", false)));
 
-        buttonPanel.add(getButton("清除日志", Main.DATA_PATH + "Log\\"));
+        //this.add(getControlUnit("日志", Main.DATA_PATH + "Log\\"));
 
-        buttonPanel.add(getButton("清理彩蛋文件", Main.TEMP_PATH + "EasterEgg\\"));
-        buttonPanel.add(getButton("清理网络文件下载缓存", Main.TEMP_PATH + "WebTemp\\"));
-        buttonPanel.add(getButton("清理帮助文档缓存", Main.TEMP_PATH + "help\\"));
+        //this.add(getControlUnit("彩蛋文件", Main.TEMP_PATH + "EasterEgg\\"));
+        //this.add(getControlUnit("网络文件下载缓存", Main.TEMP_PATH + "WebTemp\\"));
+        //this.add(getControlUnit("帮助文档缓存", Main.TEMP_PATH + "help\\"));
 
-        this.add(buttonPanel);
+        //this.add(buttonPanel);
     }
 
-    private CTTextButton getButton(String name, String DATA_PATH) throws MalformedURLException {
-        CTTextButton button = new CTTextButton(name/*, GetIcon.getIcon(Main.class.getResource("/image/light/delete_0.png"), 30, 30)*/);
+    private DataControlUnit getControlUnit(String name, String DATA_PATH, boolean canDelete, DataControlUnit... childUnits) throws MalformedURLException {
+        /*CTTextButton button = new CTTextButton(name*//*, GetIcon.getIcon(Main.class.getResource("/image/light/delete_0.png"), 30, 30)*//*);
         button.setFont(CTFont.getCTFont(Font.PLAIN, CTFontSizeStyle.SMALL));
         button.addActionListener(e -> {
             deleteDir(DATA_PATH);
         });
-        return button;
+        return button;*/
+
+        return new DataControlUnit(name, DATA_PATH, canDelete, childUnits);
 
     }
 
---
+++ src/com/wmp/classtools/infSet/panel/tools/DataControlUnit.java
@@ -0,0 +1,168 @@
+package com.wmp.classTools.infSet.panel.tools;
+
+import com.wmp.PublicTools.OpenInExp;
+import com.wmp.PublicTools.io.IOForInfo;
+import com.wmp.PublicTools.printLog.Log;
+import com.wmp.classTools.CTComponent.CTTextButton;
+import com.wmp.classTools.CTComponent.CTTextField;
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeMap;
+
+public class DataControlUnit extends JPanel {
+
+    private String name = "name";
+    private String path = "";
+    private boolean canDelete = false;
+    private List<DataControlUnit> specialChildPathList = new ArrayList<>();
+
+    /**
+     * 构造函数
+     *
+     * @param name              名字
+     * @param path              路径
+     * @param canDelete         是否可以删除
+     * @param specialChildPaths 特殊子路径列表(有传递性),不省略父路径
+     */
+    public DataControlUnit(String name, String path, boolean canDelete, DataControlUnit... specialChildPaths) {
+
+        this.name = name;
+        this.path = path;
+        this.canDelete = canDelete;
+        this.specialChildPathList.addAll(List.of(specialChildPaths));
+
+        this.setBorder(BorderFactory.createTitledBorder(name));
+        this.setLayout(new BorderLayout(5, 5));
+
+        //路径显示
+        {
+            JPanel pathPanel = new JPanel(new GridBagLayout());
+            GridBagConstraints gbc = new GridBagConstraints();
+            gbc.fill = GridBagConstraints.HORIZONTAL;
+            gbc.gridy = 0;
+            gbc.gridx = 0;
+
+            JLabel pathLabel = new JLabel("路径: ");
+            CTTextField pathTextField = new CTTextField(path);
+            pathTextField.setColumns(20);
+            pathTextField.setEditable(false);
+            CTTextButton openInExp = new CTTextButton("打开");
+            openInExp.addActionListener(e -> {
+                OpenInExp.open(path);
+            });
+            pathPanel.add(pathLabel, gbc);
+            gbc.gridx++;
+            pathPanel.add(pathTextField, gbc);
+            gbc.gridx++;
+            pathPanel.add(openInExp, gbc);
+
+            this.add(pathPanel, BorderLayout.CENTER);
+        }
+
+        // 管理按钮
+        {
+            JPanel buttonPanel = new JPanel();
+            buttonPanel.setLayout(new GridLayout(1, 0, 10, 10));
+            CTTextButton deleteButton = new CTTextButton("删除");
+            deleteButton.addActionListener(e -> {
+                try {
+                    IOForInfo.deleteDirectoryRecursively(Path.of(path));
+                } catch (IOException ex) {
+                    Log.err.print("文件管理", "删除失败:\n" + ex.getMessage());
+                    throw new RuntimeException(ex);
+                }
+            });
+            deleteButton.setEnabled(canDelete);
+            deleteButton.setForeground(Color.RED);
+
+            CTTextButton childPathControlButton = new CTTextButton("子目录管理");
+            File file = new File(path);
+            if (file.exists() && file.isDirectory()) {
+                childPathControlButton.addActionListener(e -> {
+                    JDialog dialog = new JDialog();
+                    dialog.setTitle("子目录管理");
+                    dialog.setModal(true);
+                    dialog.setLayout(new BorderLayout());
+
+
+                    JPanel panel = new JPanel(new GridLayout(0, 1));
+
+                    File[] files = file.listFiles();
+                    TreeMap<String, DataControlUnit> unitMap = new TreeMap<>();
+                    specialChildPathList.forEach(dataControlUnit -> {
+                        unitMap.put(dataControlUnit.getPath(), dataControlUnit);
+                    });
+                    Set<String> keySet = unitMap.keySet();
+                    for (File file1 : files) {
+                        DataControlUnit dataControlUnit = new DataControlUnit(file1.getName(), file1.getAbsolutePath(), false);
+                        if (keySet.contains(file1.getAbsolutePath())) {
+                            dataControlUnit = unitMap.get(file1.getAbsolutePath());
+                            //System.err.println(file1.getAbsolutePath());
+                        }
+
+                        panel.add(dataControlUnit);
+                    }
+
+                    JScrollPane scrollPane = new JScrollPane(panel);
+                    //设置滚轮灵活度
+                    scrollPane.getVerticalScrollBar().setUnitIncrement(16);
+                    dialog.add(scrollPane, BorderLayout.CENTER);
+
+                    dialog.pack();
+                    dialog.setLocationRelativeTo(this);
+                    dialog.setVisible(true);
+                });
+            } else childPathControlButton.setEnabled(false);
+
+            buttonPanel.add(deleteButton);
+            buttonPanel.add(childPathControlButton);
+
+            this.add(buttonPanel, BorderLayout.SOUTH);
+        }
+
+
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public boolean isCanDelete() {
+        return canDelete;
+    }
+
+    public void setCanDelete(boolean canDelete) {
+        this.canDelete = canDelete;
+    }
+
+    public List<DataControlUnit> getSpecialChildPathList() {
+        return specialChildPathList;
+    }
+
+    public void setSpecialChildPathList(List<DataControlUnit> specialChildPathList) {
+        this.specialChildPathList = specialChildPathList;
+    }
+}
+
+
